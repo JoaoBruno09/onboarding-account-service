4f2a95445781296b73f3bad055090f30a634b869
JoaoBruno1220256 <1220256@isep.ipp.pt>
2024-05-07 20:00:56 +0100

    Created controller to get all cards

--- web/src/main/java/com/bank/onboarding/accountservice/controllers/AccountController.java
+++ web/src/main/java/com/bank/onboarding/accountservice/controllers/AccountController.java
@@ -2,7 +2,9 @@ package com.bank.onboarding.accountservice.controllers;
 
 
 import com.bank.onboarding.commonslib.persistence.models.Account;
+import com.bank.onboarding.commonslib.persistence.models.Card;
 import com.bank.onboarding.commonslib.persistence.services.AccountService;
+import com.bank.onboarding.commonslib.persistence.services.CardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,15 @@ import java.util.List;
 public class AccountController {
 
     private final AccountService accountService;
+    private final CardService cardService;
 
-    @GetMapping("/test")
-    public List<Account> index() {
+    @GetMapping("/test/accounts")
+    public List<Account> getAccounts() {
        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/test/cards")
+    public List<Card> getCards() {
+        return cardService.getAllCards();
     }
 }
