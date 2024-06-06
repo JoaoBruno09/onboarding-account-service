68b3828bce27d3e6b024ea2ca28c74f08c9d89b0
JoaoBruno1220256 <1220256@isep.ipp.pt>
2024-06-06 23:48:04 +0100

    Added validatons to DTOS

--- web/src/main/java/com/bank/onboarding/accountservice/controllers/AccountController.java
+++ web/src/main/java/com/bank/onboarding/accountservice/controllers/AccountController.java
@@ -3,9 +3,6 @@ package com.bank.onboarding.accountservice.controllers;
 
 import com.bank.onboarding.accountservice.services.AccountService;
 import com.bank.onboarding.commonslib.persistence.exceptions.OnboardingException;
-import com.bank.onboarding.commonslib.persistence.models.Account;
-import com.bank.onboarding.commonslib.persistence.models.Card;
-import com.bank.onboarding.commonslib.persistence.services.CardRepoService;
 import com.bank.onboarding.commonslib.web.dtos.account.AccountCardDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.AccountDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.AccountDeleteCardDTO;
@@ -13,11 +10,11 @@ import com.bank.onboarding.commonslib.web.dtos.account.AccountNetbancoDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.AccountTypeRequestDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.CardDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.CreateAccountRequestDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,30 +24,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("account")
 @RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
-    private final CardRepoService cardRepoService;
 
     private static final String ACCOUNT_ID_PATH_PARAM = "/{accountNumber}";
 
-    @GetMapping("/test/accounts")
-    public List<Account> getAccounts() {
-       return accountService.getAllAccounts();
-    }
-
-    @GetMapping("/test/cards")
-    public List<Card> getCards() {
-        return cardRepoService.getAllCards();
-    }
-
     @PostMapping
-    public ResponseEntity<?> createAccount(@RequestBody CreateAccountRequestDTO createAccountRequestDTO){
+    public ResponseEntity<?> createAccount(@RequestBody @Valid CreateAccountRequestDTO createAccountRequestDTO){
         try {
             final AccountDTO accountDTO = accountService.createAccount(createAccountRequestDTO);
             return new ResponseEntity<>(accountDTO, HttpStatus.OK);
@@ -62,7 +46,7 @@ public class AccountController {
 
     @PatchMapping(ACCOUNT_ID_PATH_PARAM)
     public ResponseEntity<AccountDTO> patchAccountType(@PathVariable("accountNumber") String accountNumber,
-                                                       @RequestBody AccountTypeRequestDTO accountTypeRequestDTO){
+                                                       @RequestBody @Valid AccountTypeRequestDTO accountTypeRequestDTO){
         try {
             final AccountDTO accountDTO = accountService.patchAccountType(accountNumber, accountTypeRequestDTO);
             if(accountDTO == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados inseridos inválidos");
@@ -75,7 +59,7 @@ public class AccountController {
 
     @PutMapping(ACCOUNT_ID_PATH_PARAM + "/card")
     public ResponseEntity<CardDTO> putAccountCard(@PathVariable("accountNumber") String accountNumber,
-                                                  @RequestBody AccountCardDTO accountCardDTO){
+                                                  @RequestBody @Valid AccountCardDTO accountCardDTO){
         try {
             final CardDTO cardDTO = accountService.putAccountCard(accountNumber, accountCardDTO);
             return new ResponseEntity<>(cardDTO, HttpStatus.CREATED);
@@ -88,7 +72,7 @@ public class AccountController {
     @DeleteMapping(ACCOUNT_ID_PATH_PARAM + "/card/{cardNumber}")
     public ResponseEntity<AccountDTO> deleteAccountCard(@PathVariable("accountNumber") String accountNumber,
                                                         @PathVariable("cardNumber") String cardNumber,
-                                                        @RequestBody AccountDeleteCardDTO accountDeleteCardDTO){
+                                                        @RequestBody @Valid AccountDeleteCardDTO accountDeleteCardDTO){
         try {
             final AccountDTO accountDTO = accountService.deleteAccountCard(accountNumber, cardNumber, accountDeleteCardDTO);
             return new ResponseEntity<>(accountDTO, HttpStatus.OK);
@@ -100,7 +84,7 @@ public class AccountController {
 
     @PutMapping(ACCOUNT_ID_PATH_PARAM + "/netbanco")
     public ResponseEntity<AccountDTO> putAccountNetbanco(@PathVariable("accountNumber") String accountNumber,
-                                                         @RequestBody AccountNetbancoDTO accountNetbancoDTO){
+                                                         @RequestBody @Valid AccountNetbancoDTO accountNetbancoDTO){
         try {
             final AccountDTO accountDTO = accountService.putAccountNetbanco(accountNumber, accountNetbancoDTO);
             return new ResponseEntity<>(accountDTO, HttpStatus.OK);
